2aa183255d6afc54aff228b802a09b9c7e9efb64
Brian Cespedes <briancespedes94@gmail.com>
2019-08-19 23:44:57 -0300

    Email Template - pyJWT

--- cride/users/serializers/users.py
+++ cride/users/serializers/users.py
@@ -2,17 +2,25 @@
 """
 
 #Django
+from django.conf import settings
 from django.contrib.auth import authenticate, password_validation
 from django.core.validators import RegexValidator
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+from django.utils import timezone
 
 # Django Rest framework
 from rest_framework.authtoken.models import Token
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
 
-
 # Models
+
 from cride.users.models import User, Profile
+
+#Utilities
+import jwt
+from datetime import timedelta
 
 class UserModelSerializer(serializers.ModelSerializer):
 	# User model serializer
@@ -64,7 +72,36 @@ class UserSignUpSerializer(serializers.Serializer):
 		data.pop('password_confirmation')
 		user = User.objects.create_user(**data, is_verified=False)
 		Profile.objects.create(user=user)
+		self.send_confirmation_email(user)
 		return user
+
+	def send_confirmation_email(self, user):
+		""" Send account verification link to given user """
+		verification_token = self.gen_verification_token(user)
+
+		subject = 'Welcome @{}! Verify yout account to start using Comparte Ride'.format(user.username)
+		from_email = 'Comparte Ride <noreply@comparteride.com>'
+		content = render_to_string(
+			'emails/users/account_verification.html', 
+			{'token': verification_token, 'user': user}
+		)
+		msg = EmailMultiAlternatives(subject, content, from_email, [user.email])
+		msg.attach_alternative(content, "text/html")
+		msg.send()
+		
+		print("Sending email")
+
+	def gen_verification_token(self, user):
+		""" Create JWT token that the user can user to verify its account."""
+		exp_date = timezone.now() + timedelta(days=3)
+		payload = {
+			'user' : user.username,
+			'exp'  : int(exp_date.timestamp()),
+			'type' : 'email_confirmation'
+		}
+		token = jwt.encode(payload, settings.SECRET_KEY, algorithm='HS256')
+		# token is a byte. Decode is not from jwt
+		return token.decode()
 
 
 
